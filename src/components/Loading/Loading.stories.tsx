28f855c6355b245a19c925388c4fb16e5646516a
zukki30 <kw4869ify@gmail.com>
2022-09-02 00:47:45 +0900

    feat: Add Loading components

---
+++ src/components/Loading/Loading.stories.tsx
@@ -0,0 +1,12 @@
+import { Loading } from "./Loading";
+
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+  title: "Loading",
+  component: Loading,
+} as ComponentMeta<typeof Loading>;
+
+const Template: ComponentStory<typeof Loading> = () => <Loading />;
+
+export const Default = Template.bind({});
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,5 @@
+import styles from "./Styles.module.css";
+
+export const Loading = () => {
+  return <div className={styles.loading} />;
+};
