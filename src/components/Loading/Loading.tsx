e1b5dbb1a66451bfac710f09e50d3978dc31cce4
zukki30 <kw4869ify@gmail.com>
2022-09-03 00:47:15 +0900

    fix: Fix Loading style

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,3 @@
 import styles from "./Styles.module.css";
 
-export const Loading = () => {
-  return <div className={styles.loading} />;
-};
+export const Loading = () => <div className={styles.loading} />;
